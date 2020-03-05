44fd5284a1635eaf983cf3b2935ac3450e3f5f66
Dmitry Fedyunin <multidimko@gmail.com>
2020-03-05 22:25:24 +0700

    (f) refactor project

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,64 +1,64 @@
 const Path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: Path.resolve(__dirname, '../src/scripts/index.js'),
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.js'),
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
     },
-    output: {
-        path: Path.join(__dirname, '../build'),
-        filename: '[name].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../public'), to: 'public' },
+    ]),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../src/icons'), to: './src/icons'}
+    ]),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/views/pages/index.pug'),
+      filename: 'index.html',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src'),
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            name: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './src/icons/[name].[ext]',
+          },
         },
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([
-            { from: Path.resolve(__dirname, '../public'), to: 'public' },
-        ]),
-        new CopyWebpackPlugin([
-            { from: Path.resolve(__dirname, '../src/icons'), to: './src/icons'}
-        ]),
-        new HtmlWebpackPlugin({
-            template: Path.resolve(__dirname, '../src/views/pages/index.pug'),
-            filename: 'index.html',
-        }),
+      },
+      {
+        test: /\.css$/i,
+        use: ['to-string-loader', 'css-loader'],
+      },
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+      },
     ],
-    resolve: {
-        alias: {
-            '~': Path.resolve(__dirname, '../src'),
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.mjs$/,
-                include: /node_modules/,
-                type: 'javascript/auto',
-            },
-            {
-                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: './src/icons/[name].[ext]',
-                    },
-                },
-            },
-            {
-                test: /\.css$/i,
-                use: ['to-string-loader', 'css-loader'],
-            },
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
+  },
 };
